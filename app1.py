9f0f788ed9ed054202404fe82613d2739b346325
Airukua <124700893+Airukua@users.noreply.github.com>
2023-07-26 15:04:18 +0900

    Update streamlit_app.py

--- app1.py
+++ app1.py
@@ -1,3 +1,4 @@
+from absl import app
 import numpy as np
 import tensorflow as tf
 import tensorflow_text as tf_text
@@ -8,26 +9,27 @@ import streamlit as st
 st.set_page_config(page_title="Sigma Ai | Aplikasi Penerjemah Bahasa Geser", page_icon="🤖")
 st.title("Demo NMT Indonesia|Geser")
 
-interpreter = tf.lite.Interpreter('converted_model.tflite')
+interpreter = tf.lite.Interpreter('/content/model_terbaru.tflite')
 interpreter.allocate_tensors()
 
-    
+# Get input and output tensors
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+def translate(user_input):
+    input_shape = input_details[0]['shape']
+    input_data = np.array([user_input], dtype=np.str)
+    return input_data
+
 # Form to add your items
 with st.form("my_form"):
     user_input = st.text_area("Masukan Kata...", max_chars=200)
-    input_details = interpreter.get_input_details()
-    output_details = interpreter.get_output_details()
 
-    input_shape = input_details[0]['shape']
-    input_data = np.array(np.random.random_sample(input_shape), dtype=np.float32)
-    input_data = [user_input]
-    input_data = np.array(input_data, dtype=str)
-    interpreter.set_tensor(input_details[0]['index'], input_data)
-    interpreter.invoke()
-    output_data = interpreter.get_tensor(output_details[0]['index'])
-    output_string = output_data[0].decode('utf-8')
-    submitted = st.form_submit_button("Translate")
-
-    if submitted:
+    # Run the model
+    if st.form_submit_button("Translate"):
         st.write("Hasil Terjemahan")
-        st.info(output_string)
+        input_data = translate(user_input)
+        interpreter.set_tensor(input_details[0]['index'], input_data)
+        interpreter.invoke()
+        output_data = interpreter.get_tensor(output_details[0]['index'])
+        st.info(output_data)
